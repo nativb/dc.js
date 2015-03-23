f9abfb9ce11c24675fcf7af48b69aff7ab750a5d
Nativ Ben-David <nativ@me.com>
2015-03-23 11:37:55 +0200

    Revert "2.01"
    This reverts commit 467462d781c3f93c8236abe4b599fd5249bd2555, reversing
    changes made to 1c7a39954510b0204b3c76175c0ae8d4e72db81c.

--- src/logger.js
+++ src/logger.js
@@ -2,13 +2,6 @@ dc.logger = {};
 
 dc.logger.enableDebugLog = false;
 
-//some changes
-
-
-//some more changes
-
-//test changes 2
-
 dc.logger.warn = function (msg) {
     if (console) {
         if (console.warn) {
@@ -20,6 +13,7 @@ dc.logger.warn = function (msg) {
 
     return dc.logger;
 };
+
 dc.logger.debug = function (msg) {
     if (dc.logger.enableDebugLog && console) {
         if (console.debug) {
@@ -31,5 +25,3 @@ dc.logger.debug = function (msg) {
 
     return dc.logger;
 };
-
-//test changes
